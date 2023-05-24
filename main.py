a84416ce4ffae5b95692df30f77a5e633c0c8ce6
rutamt <95180713+rutamt@users.noreply.github.com>
2023-05-24 18:25:27 -0400

    Done. Final commit. Changed the CSS a bit

--- main.py
+++ main.py
@@ -18,10 +18,10 @@ from moviepy.editor import (
     concatenate_videoclips,
     AudioFileClip,
     CompositeAudioClip,
-    vfx,
 )
 from concurrent.futures import ThreadPoolExecutor
 import random
+from flask import Flask, render_template, request, redirect
 
 nltk.download("punkt")
 nltk.download("averaged_perceptron_tagger")
@@ -31,6 +31,9 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 pexels_api_key = os.getenv("PEXELS_API_KEY")
 # Where pexels stock footage will be downloaded to
 download_folder = "stock_footage"
+os.makedirs(download_folder, exist_ok=True)
+static_folder = "static"
+os.makedirs(static_folder, exist_ok=True)
 
 
 # Returns a list of nouns in a sentence provided
@@ -49,11 +52,11 @@ def extract_nouns(paragraph):
         pos_tags = nltk.pos_tag(words)
 
         for word, pos in pos_tags:
-            # Check if the word is a noun
-            if pos.startswith("NN"):
+            # Check if the word is a noun and not already found in the sentence
+            if pos.startswith("NN") and word not in nouns:
                 # Add the noun to the list
                 nouns.append(word)
-                break  # Break out of the loop after finding the first noun
+                break
 
     return nouns
 
@@ -119,10 +122,17 @@ def download_and_process_video(noun, duration):
 
     # Add the trimmed video clip to the clips list
     clips.append(video_clip)
+    video_clip.close()
     # closing video_filename before deletion
     f.close()
     # Clean up the video file
-    os.remove(video_filename)
+    # Clean up the video file
+    try:
+        os.remove(video_filename)
+        print(f"{video_filename} removed successfully")
+    except OSError as error:
+        print(error)
+        print("File path can not be removed")
 
 
 print("clips: ", clips)
@@ -140,37 +150,32 @@ def get_sentence_duration(sentence):
 
 
 # Returns the sentence that contains the noun
-def find_sentence_with_noun(noun):
-    sentences = generated_script.split(".")
+def find_sentence_with_noun(noun, script):
+    sentences = script.split(".")
     for sentence in sentences:
         if noun.lower() in sentence.lower():
             return sentence.strip()
     return None
 
 
-# The script for everything
-generated_script = "In a world filled with constant motion, take a moment to pause. Close your eyes and let your imagination soar."
-
 # Passing the text and language to the engine,
 # Language in which you want to convert
-language = "en"
-myobj = gTTS(text=generated_script, lang=language, slow=False)
-# Saving the converted audio in a mp3 file named welcome
-myobj.save("voiceover.mp3")
 
 # The text that you want to convert to audio
 # article_topic = input("What would you like the article to be about? ")
 
 
 #  Combines all the mp4 files in a folder
-def concatenate_mp4_files(folder="stock_footage", voiceover_file="voiceover.mp3"):
+def concatenate_mp4_files(
+    nouns, folder="stock_footage", voiceover_file="voiceover.mp3"
+):
     clips = []
 
     # Get a list of all MP4 files in the folder
     file_list = [file for file in os.listdir(folder) if file.endswith(".mp4")]
 
-    # Sort the file list based on the creation time
-    file_list.sort(key=lambda file: os.path.getctime(os.path.join(folder, file)))
+    # Sort the file list based on the order of the nouns list
+    file_list.sort(key=lambda file: nouns.index(file.split("_")[1].split(".")[0]))
 
     # Get the resolution of the first video clip
     first_clip_path = os.path.join(folder, file_list[0])
@@ -197,7 +202,7 @@ def concatenate_mp4_files(folder="stock_footage", voiceover_file="voiceover.mp3"
         clips.append(trimmed_clip)
 
     # Concatenate the clips
-    final_clip = concatenate_videoclips(clips)
+    final_clip = concatenate_videoclips(clips, method="compose")
 
     # Load the voiceover file as an AudioFileClip
     voiceover_clip = AudioFileClip(voiceover_file)
@@ -220,56 +225,86 @@ def concatenate_mp4_files(folder="stock_footage", voiceover_file="voiceover.mp3"
     combined_audio = CompositeAudioClip(
         [music_clip.volumex(0.6), voiceover_clip.volumex(1.0)]
     )
+
     # Set the audio of the final video clip to the combined audio
     # Trim the final video clip based on the duration of the voiceover
-    final_clip = final_clip.subclip(
-        0, min(final_clip.duration, voiceover_clip.duration)
-    )
     final_clip = final_clip.set_audio(combined_audio)
 
-    # Adjust the duration of the final video clip to match the voiceover duration
-
-    # Set the audio of the final video clip to the combined audio
+    # Set the duration of the final video clip based on the longer duration
+    final_duration = max(final_clip.duration, voiceover_clip.duration)
+    final_clip = final_clip.set_duration(final_duration)
 
     # Define the output file path for the final video
-    output_filename = "video_no_sound.mp4"
+    output_filename = os.path.join(static_folder, "concatenated_video.mp4")
 
     # Write the final video file
     final_clip.write_videofile(
-        output_filename, codec="libx264", audio_codec="aac", fps=30, remove_temp=False
+        output_filename,
+        codec="libx264",
+        audio_codec="aac",
+        fps=30,
+        remove_temp=False,
     )
-
-    # for clip in clips:
-    #     clip.close()
-
-    # # Delete the video files from the folder
-    # for file in file_list:
-    #     filepath = os.path.join(folder, file)
-    #     os.remove(filepath)
 
     return final_clip
 
 
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def input_page():
+    if request.method == "POST":
+        start_time = time.time()
+        generated_script = request.form["generated_script"]
+        print(generated_script)
+        # Call the function that processes the generated_script and creates the video
+        # generated_script = "In a world filled with constant motion, take a moment to pause. Close your eyes and let your imagination soar."
+        language = "en"
+        myobj = gTTS(text=generated_script, lang=language, slow=False)
+        # Saving the converted audio in a mp3 file named welcome
+        myobj.save("voiceover.mp3")
+
+        nouns = extract_nouns(generated_script)
+        print("Nouns: ", nouns)
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            futures = []
+            for noun in nouns:
+                duration = get_sentence_duration(
+                    find_sentence_with_noun(noun, generated_script)
+                )
+                future = executor.submit(download_and_process_video, noun, duration)
+                futures.append(future)
+
+            for future in concurrent.futures.as_completed(futures):
+                clip = future.result()
+                if clip:
+                    clips.append(clip)
+
+        # Check if any clips were found before concatenating
+        if clips:
+            # Concatenate all the clips together
+            concatenate_mp4_files(nouns)
+        # Return a redirect to the video display page
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print("Video processing completed in {:.2f} seconds.".format(elapsed_time))
+
+        return redirect("/video")
+
+    # Render the input page template
+    return render_template("input.html")
+
+
+@app.route("/video")
+def video_page():
+    # Render the video display page template
+
+    return render_template("video.html")
+
+
 # generated_script = generate_script(article_topic)
+# The script for everything
 
-nouns = extract_nouns(generated_script)
-print("Nouns: ", nouns)
-with concurrent.futures.ThreadPoolExecutor() as executor:
-    futures = []
-    for noun in nouns:
-        duration = get_sentence_duration(find_sentence_with_noun(noun))
-        future = executor.submit(download_and_process_video, noun, duration)
-        futures.append(future)
-
-    for future in concurrent.futures.as_completed(futures):
-        clip = future.result()
-        if clip:
-            clips.append(clip)
-
-
-# Check if any clips were found before concatenating
-if clips:
-    # Concatenate all the clips together
-    concatenated_clip = concatenate_mp4_files()
-    output_filename = "concatenated_video.mp4"
-    concatenated_clip.write_videofile(output_filename, codec="libx264")
+if __name__ == "__main__":
+    app.run(debug=True)
